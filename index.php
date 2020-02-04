bb0a139130b48818951e55166942a8a730ca3666
juanmacivico87 <juanmacivico87@gmail.com>
2020-02-04 17:30:58 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+        <meta charset="utf-8">
+        <title>Nearest places</title>
+        <link rel="stylesheet" type="text/css" href="inc/css/styles.css">
+    </head>
+    <body>
+        <div id="map"></div>
+        <form action="" method="post">
+            <input type="text" name="address" placeholder="Address">
+            <button type="button" id="search" name="search">Send</button>
+        </form>
+        <div id="results"></div>
+    </body>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <script src="inc/js/scripts.js" type="text/javascript"></script>
+</html>
---
+++ readme.md
@@ -0,0 +1,7 @@
+#Google Maps - Centers Searcher
+
+A script to show a map and search for centers saved in a JSON.
+
+It uses the Google Maps API for JavaScript, so for it to work it is necessary to have an API key for this tool.
+
+The script has no defined style, only the one that causes the map to be displayed. Feel free to edit it and adapt it to the needs of your project.
